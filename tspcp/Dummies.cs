b73fb9b3de84f60cafe15fabff085b181f0ef0d1
bloody <mail@defaude.info>
2010-01-28 17:08:22 +0000

    Simple implementation done, including pseudo-test class

---
+++ tspcp/Dummies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace tspcp
+{
+    /// <summary>
+    /// This class implements the queue producer for string tasks.
+    /// 
+    /// We only need to pass down the queue object to the base class's constructor and override the produce method!
+    /// </summary>
+    class DummyProducer : AbstractQueueProducer<string>
+    {
+        public DummyProducer(SynchronizedQueue<string> queue) : base(queue) { }
+
+        protected override string produce()
+        {
+            Thread.Sleep(200);
+            return "tasky";
+        }
+    }
+
+    /// <summary>
+    /// This class implements the queue consumer for string tasks.
+    /// 
+    /// We only need to pass down the queue object to the base class's constructor and override the consume method!
+    /// </summary>
+    class DummyConsumer : AbstractQueueConsumer<string>
+    {
+        public DummyConsumer(SynchronizedQueue<string> queue) : base(queue) { }
+
+        protected override void consume(string task)
+        {
+            Console.WriteLine(task);
+            Thread.Sleep(500);
+        }
+    }
+}
--- tspcp/TestMain.cs
+++ tspcp/TestMain.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Threading;
 
 namespace tspcp
 {
+    /// <summary>
+    /// Simple test class that uses the classes in Dummies.cs to test the behaviour of the AbstractQueueConsumer, AbstractQueueProducer and SynchronizedQueue classes.
+    /// </summary>
     class TestMain
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-            Console.WriteLine("Goodbye, world!");
+
+            // This using block defines the lifetime of the queue and therefore the lifetime of all producers and consumers
+            using (SynchronizedQueue<string> qs = new SynchronizedQueue<string>(true))
+            {
+                // Now we're creating several producers and consumers
+                AbstractQueueProducer<string> producer1 = new DummyProducer(qs);
+                AbstractQueueProducer<string> producer2 = new DummyProducer(qs);
+                AbstractQueueProducer<string> producer3 = new DummyProducer(qs);
+                AbstractQueueProducer<string> producer4 = new DummyProducer(qs);
+                AbstractQueueConsumer<string> consumer1 = new DummyConsumer(qs);
+                AbstractQueueConsumer<string> consumer2 = new DummyConsumer(qs);
+                AbstractQueueConsumer<string> consumer3 = new DummyConsumer(qs);
+                AbstractQueueConsumer<string> consumer4 = new DummyConsumer(qs);
+                AbstractQueueConsumer<string> consumer5 = new DummyConsumer(qs);
+
+                // All consumers/producers have been created. Now let's have the main thread rest for a while.
+                Thread.Sleep(5000);
+
+                // Alright, enough sleeping for the main thread! Let's get out of this using block and therefore kill all consumers/workers and the queue itself!
+            }
+
+            Console.WriteLine("Goodbye, world! (press enter)");
             Console.Read();
         }
     }
