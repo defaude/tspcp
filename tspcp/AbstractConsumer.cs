45dc25fea315fd5c77b7ac2b3830937e14493a0b
bloody <mail@defaude.info>
2010-01-28 10:15:02 +0000

    Set up TestMain.cs

---
+++ tspcp/AbstractConsumer.cs
@@ -0,0 +1,7 @@
+﻿
+namespace tspcp
+{
+    class AbstractConsumer
+    {
+    }
+}
---
+++ tspcp/TestMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tspcp
+{
+    class TestMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Goodbye, world!");
+            Console.Read();
+        }
+    }
+}
